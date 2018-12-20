490b7e6555c860c10c95694e665f47a76869d569
em <6em.rva@gmail.com>
2018-12-19 20:15:50 -0500

    start on date reformat

--- assets/js/article.js
+++ assets/js/article.js
@@ -12,23 +12,19 @@
     function getQueryURL() {
         // API query url stored inside a variable
         var queryURL = 'https://api.nytimes.com/svc/topstories/v2/';
+        // store the #sectionChoice value in a variable to add to the queryURL
+        var sectionChoice = $('#sectionChoice').val().trim();
         // params order: API key, search term, begin date, end date, sort
         var queryParams = { 'api-key': 'c43fdcb0dd074a0bb172684363bdf7bd' } 
-        
-        // store the #sectionChoice value in a variable to add to the queryURL
-        var sectionChoice = $('#sectionChoice').val();
-        
         // return the query URL with the paramaters according to the NYT API docs
-        console.log(queryURL + sectionChoice + '.json?' + $.param(queryParams));
         return queryURL + sectionChoice + '.json?' + $.param(queryParams);
     }
 
+    // an empty array to hold the new filtered results
+    var filteredResults = [];
     // search JSON results for text search input, filters results
     // filters results based on the newer or older sortChoice
     function filterResults(result) {
-
-        // an empty array to hold the new filtered results
-        var filteredResults = [];
         // results is the location of the search results in the json object
         var results = result.results;
         // if the #searchQuery is not equal to null, make all letters lowercase 
@@ -44,34 +40,47 @@
                 var resTitle = (results[i].title).toLowerCase();
                 var resAbstract = (results[i].abstract).toLowerCase();
 
-                // if statement to check if the searchQuery matches anything from 
-                // resTitle OR resAbstract
+                // if statement to check if the searchQuery matches anything from resTitle OR resAbstract
                 if ((resTitle.indexOf(searchQuery) > -1) || (resAbstract.indexOf(searchQuery) > -1)) {
                     // push the results of the search to the filteredResults array
                     filteredResults.push(results[i]);
                 }
             }
-        }
-        console.log(filteredResults);
-        // once the results have been filtered, they will be sorted based on the
-        // sort choice, either by newest or oldest published date
-        
-        // the value of the sortChoice selection is stored in a variable
-        var sortChoice = $('#sortChoice').val();
-        // the sortChoice is tested to see if it equals newest or oldest
-        if ( sortChoice === 'newest' ) {
-            console.log(newest)
-        } else if ( sortChoice === 'oldest' ) {
-            console.log(oldest)
-        } //else (
-            
-        // )
-    
+        } 
+        return filteredResults;
     }
-
-
     // update content of page to show new results
+    function updateContent () {
+        // a loop to define the number of articles, reformat each publish date, and build a card for each article
+        for (var i = 0; i < filteredResults.length; i++) {
+            // number of results
+            var numResults = filteredResults.length;
+            // article number
+            var numArticle = i + 1;
+            // vars for the title, abstract, URL, published date, and image URL
+            var title = filteredResults[i].title;
+            var abstract = filteredResults[i].abstract;
+            var articleURL = filteredResults[i].short_url;
+            var imageURL = filteredResults[i].multimedia[0].url;
+            // this is where the date is reformatted
+            var published = filteredResults[i].published_date;
+            // split the published_date string
+            var date = published.split('');
+                console.log(date);
+            
+            // console.log(numArticle + '\n' + title + '\n' + abstract + '\n' + articleURL + '\n' + imageURL + '\n' + published);
 
+            
+            // function formatPublished(published) {
+                
+            // }
+            // formatPublished();
+            // if there is an imageURL add image to card
+            // if (imageURL !== undefined) {
+
+            // } 
+        }
+    }
 
 
 //    click handler listening for the search button click to 
@@ -85,6 +94,8 @@
         event.preventDefault();
         // call the clear function to empty the results div
         clear();
+        // empty the filteredResults div
+        filteredResults = [];
         // call getQueryURL function to gather paramaters for the AJAX request 
         // to the NYT API, put that in a variable
         var queryURL = getQueryURL();
@@ -98,7 +109,7 @@
             // search terms and sort choice
             // console.log(result);
             filterResults(result)
-        })//.then(updateContent); // then the fresh data gets passed as an argument to the updateContent function
+        }).then(updateContent); // then the fresh data gets passed as an argument to the updateContent function
     });
 
 
